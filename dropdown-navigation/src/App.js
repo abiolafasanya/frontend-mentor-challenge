863c40880abf145fb2c9581b85942eb6b01d924d
Abiola Fasanya <hezekiahfasanya@gmail.com>
2022-04-26 17:22:38 +0000

    commit started

---
+++ dropdown-navigation/src/App.js
@@ -0,0 +1,14 @@
+import NavBar from './components/navBar/NavBar';
+
+function App() {
+  return (
+    <div className="container">
+      <div className='mx-auto w-3/4'>
+        <NavBar />
+        <h1>Hello World</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
